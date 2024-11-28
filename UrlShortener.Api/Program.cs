f0e61866afab4a6f167d65aa67d5b7c04ed4953b
Thorsteinn Gestsson <t19n@outlook.com>
2024-11-28 15:48:59 +0100

    use keyvault on api

--- UrlShortener.Api/Program.cs
+++ UrlShortener.Api/Program.cs
@@ -1,4 +1,14 @@
+using Azure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var keyVaultName = builder.Configuration["KeyVaultName"];
+if (!string.IsNullOrEmpty(keyVaultName))
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{keyVaultName}.vault.azure.net/"),
+        new DefaultAzureCredential());
+}
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
